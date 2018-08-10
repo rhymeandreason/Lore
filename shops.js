865f6413661050605c05871cb1df7461cb44b8c0
rhymeandreason <rhymeandreason1.0@gmail.com>
2018-08-10 16:31:08 -0700

    Added controls for Location toggle
    also added cupcake shop

--- shops.js
+++ shops.js
@@ -20,7 +20,7 @@ var pizza_shop = {
   "pizza7": 15,
   "pizza8": 18,
   "pizza9": 18,
-  "pizza10": 18,
+  "pizza10": 18
 }
 
 var dimsum_shop = {
@@ -35,6 +35,55 @@ var dimsum_shop = {
   "dimsum9": 4,
   "dimsum10": 4,
   "dimsum11": 4,
-  "dimsum12": 4,
+  "dimsum12": 4
+}
 
+var sushi_shop = {
+  "sushi1": 1,
+  "sushi2": 1,
+  "sushi3": 1,
+  "sushi4": 1,
+  "sushi5": 1,
+  "sushi6": 3,
+  "sushi7": 2,
+  "sushi8": 1,
+  "sushi9": 2,
+  "sushi10": 2,
+  "sushi11": 2,
+  "sushi12": 2,
+  "sushi13": 2,
+  "sushi14": 1,
+  "sushi15": 1,
+  "sushi16": 1,
+  "sushi17": 1,
+  "sushi18": 1,
+  "sushi19": 1,
+  "sushi20": 2,
+  "sushi21": 3,
+  "sushi22": 3,
+  "sushi23": 3
+}
+
+var taco_shop = {
+  "taco1": 2,
+  "taco2": 3,
+  "taco3": 3,
+  "taco4": 2,
+  "taco5": 2,
+  "taco6": 4,
+  "taco7": 3,
+  "taco8": 3,
+  "taco9": 4
+}
+
+var cupcake_shop = {
+  "cupcake1": 3,
+  "cupcake2": 3,
+  "cupcake3": 3,
+  "cupcake4": 3,
+  "cupcake5": 3,
+  "cupcake6": 3,
+  "cupcake7": 3,
+  "cupcake8": 3,
+  "cupcake9": 3,
 }
