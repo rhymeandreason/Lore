643a79e5ca53b2d940636c40d1db757120dd1490
rhymeandreason <rhymeandreason1.0@gmail.com>
2018-08-10 18:12:44 -0700

    add button for ClearData

--- place.js
+++ place.js
@@ -1,7 +1,7 @@
 function check_quest_progress(place){
   var section = $("#"+place + " .npc-section");
 
-  if (NPC['Spelunker'].location == null && $("#"+place).hasClass('landmark')){
+  if (!Player.quests_progress.hasOwnProperty("Oak-ness Monster") && $("#"+place).hasClass('landmark')){
     //first landmark
     var npc = "<span class='npc_icon animated bounceInRight delay-2s Spelunker' onclick='NPC_chat(\"Spelunker\")'><img src='npc/spelunker.svg' /><img class='chatty' src='icons/chatty.svg'/></span>";
     //need code to remove from previous landmarks if you don't click him at the first landmark? Or when we have more quests he can spawn in a specific location
