5efa750b662e647a2ee2d3588eed608c27c40da0
rhymeandreason <rhymeandreason1.0@gmail.com>
2018-08-18 18:07:13 -0700

    dynamically color close button

--- place.js
+++ place.js
@@ -38,3 +38,59 @@ function generic_chat(el){
   //console.log(el);
   $(el).find('.small-talk').toggle();
 }
+var canvas;
+
+function getImageLightness(imageSrc,callback) {
+    var img = document.createElement("img");
+    img.src = imageSrc;
+    img.style.display = "none";
+    document.body.appendChild(img);
+
+    var colorSum = 0;
+    var w, h;
+
+    img.onload = function() {
+        // create canvas
+        canvas = document.createElement("canvas");
+
+        canvas.width = this.width;
+        canvas.height = this.height;
+        w = this.width;
+        h = this.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this,0,0);
+
+        var imageData = ctx.getImageData(w-50,0,w,50);
+        var data = imageData.data;
+        var r,g,b,avg;
+
+        for(var i = 0, len = data.length; i < len; i+=4) {
+          var brightness = .34*data[i] + .5*data[i+1] + .16 * data[i+2];
+            //r = brightness;
+            //g = brightness;
+            //b = brightness;
+            //avg = Math.floor((r+g+b)/3);
+            colorSum += brightness;
+        }
+        //document.body.appendChild(canvas);
+        var brightness = Math.floor(colorSum / (50*50));
+        callback(brightness);
+    }
+}
+
+function setIcon(val){
+  console.log("brightness: "+ val);
+  if (val>180){
+    $("#close_location").removeClass("background--dark");
+    $("#close_location").addClass("background--light");
+  } else {
+    $("#close_location").addClass("background--dark");
+    $("#close_location").removeClass("background--light");
+  }
+}
+function seeCanvas(){
+
+        window.open(canvas.toDataURL("image/png"), '_blank');
+
+}
