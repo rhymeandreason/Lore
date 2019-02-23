dc1d5285ef65e6fe644d8170066a0ac7987bc02e
rhymeandreason <rhymeandreason1.0@gmail.com>
2019-02-22 21:56:24 -0800

    try to fix rotation

--- three/DeviceOrientationControls.js
+++ three/DeviceOrientationControls.js
@@ -49,7 +49,7 @@ THREE.DeviceOrientationControls = function ( object ) {
 
 			quaternion.setFromEuler( euler ); // orient the device
 
-			quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
+			//quaternion.multiply( q1 ); // camera looks out the back of the device, not the top
 
 			quaternion.multiply( q0.setFromAxisAngle( zee, - orient ) ); // adjust for screen orientation
 
